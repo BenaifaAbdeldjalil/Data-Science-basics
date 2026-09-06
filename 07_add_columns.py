d239392bae77b2931fd30bf743acf45f8b4521ad
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-06 23:48:42 +0200

    add  modifications 07_add_columns.py

--- 07_add_columns.py
+++ 07_add_columns.py
@@ -4,38 +4,27 @@ import pandas as pd
 #read csv file and put it in variable(df)
 df = pd.read_csv("dataset/data.csv")
 
-
-#nul values
-#print(df.isnull())
-
-#notnul values
-#print(df.notnull())
-
-#one column
-filter = df['tax'].notnull()
-#df_nan= df[df['tax'].notnull()]
-#print(df_nan)
+# replace nan with 0
+df["tax"]=df["tax"].fillna(0, inplace=True)
 
 
-#replace null values
-#df["tax"]= df["tax"].fillna(0)
-#print(df)
+#convert 
+df.price_paid=df.price_paid.apply(lambda x: x.replace("$",""))
+df.price_paid=df.price_paid.astype(float)
 
-
-#replace null values
-#df["tax"]= df["tax"].bfill()
-# df.fillna(method="ffill")   # ❌ TypeError dans pandas récent
-#df.fillna(method="bfill")   # ❌
+#add column df["tax_price"]
+df["tax_price"]=round(df["price_paid"]*(1-df["tax"]/100),2)
 print(df)
 
 
-#remouve null values
-df_nan= df.dropna(subset=["tax"]) 
-print(df_nan) #[654 rows x 10 columns]
 
 
-df_nann= df.dropna(subset=["tax","country"]) 
-print(df_nann) #[628 rows x 10 columns]
+
+
+
+
+
+
 
 
 
