20fb5307c8f8db70b34de90711a72778fc91d0d2
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-06 15:55:58 +0200

    Add lines file 02_Dataframe_analysis.py

--- 02_Dataframe_analysis.py
+++ 02_Dataframe_analysis.py
@@ -11,4 +11,34 @@ df = pd.read_csv("dataset/data.csv")
 #print(df.tail())
 
 #haed with more details ex: 8 firts lines
-print(df.head(8))
+#print(df.head(8))
+
+#shape of dataset
+print(df.shape) # (1000, 10)
+
+#columns
+#print(df.columns) #Index(['id', 'date', 'first_name', 'last_name', 'email', 'gender', 'ip_address', 'country', 'price_paid', 'tax'], dtype='str')
+
+#columns on liste
+print(df.columns.tolist()) #['id', 'date', 'first_name', 'last_name', 'email', 'gender', 'ip_address', 'country', 'price_paid', 'tax']
+
+
+#index
+print(df.index) #RangeIndex(start=0, stop=1000, step=1)
+
+
+#modify index, inplace = True modify the original dataframe
+df.set_index('id', inplace=True)
+#print(df)
+
+
+print(df.index)
+
+
+
+
+
+
+
+
+
