6c4e2c3d803937d6275352b468561ae4244ee856
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-06 15:30:17 +0200

    Add lines file 02_Dataframe_analysis.py

--- 02_Dataframe_analysis.py
+++ 02_Dataframe_analysis.py
@@ -4,4 +4,11 @@ import pandas as pd
 #read csv file and put it in variable(df)
 df = pd.read_csv("dataset/data.csv")
 
-print(df)
+#haed 5 firts lines
+#print(df.head())
+
+#tail 5 latest lines
+#print(df.tail())
+
+#haed with more details ex: 8 firts lines
+print(df.head(8))
