ea6ed81e3301c6dfbf4e8c78f4247c41eabdf004
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-06 19:24:05 +0200

    add  modifications on 05_drop_column.py

---
+++ 05_drop_column.py
@@ -0,0 +1,17 @@
+#import pandas modul
+import pandas as pd
+
+#read csv file and put it in variable(df)
+df = pd.read_csv("dataset/data.csv")
+
+#Drop axis=1 --> column, axis=0 --> row
+#Drop column "ip_adress"
+df.drop("ip_address", axis=1, inplace=True)
+print(df.columns) #Index(['id', 'date', 'first_name', 'last_name', 'email', 'gender', 'country','price_paid', 'tax'],
+#print(df)
+
+#Drop lines "Male"
+df.set_index("gender",inplace=True)
+df.drop("Male",axis=0,inplace=True)
+print(df) #[562 rows x 8 columns]
+
