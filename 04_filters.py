69e639d0cb9a601f5fdf6217459bca8afd6e4f34
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-06 17:08:08 +0200

    add  modifications on 04_filters.py

---
+++ 04_filters.py
@@ -0,0 +1,42 @@
+#import pandas modul
+import pandas as pd
+
+#read csv file and put it in variable(df)
+df = pd.read_csv("dataset/data.csv")
+
+#haed 5 firts lines
+#print(df.head())
+
+
+#select email column
+#print(df["email"].head())
+
+#or 
+print(df.email.head())
+
+#dataframe vs serie
+#dataframe 
+print(type(df)) #<class 'pandas.DataFrame'>
+
+#Serie
+print(type(df.email)) #<class 'pandas.Series'>
+
+#data from 10 to 20 lines
+print(df[10:20]) #line 20 is not in the list
+
+#with LOC with index numeric
+print(df.loc[10:20]) #line 20 is in the list
+
+#with LOC with index varchar
+df_email=df.set_index("email")
+#print(df_email.head(10))
+#print(df_email.loc['hharridge1@gnu.org']) #
+#print(type(df_email.loc['hharridge1@gnu.org'])) #<class 'pandas.Series'>
+
+#just values
+#print(df_email.loc['hharridge1@gnu.org'].values)  #array
+
+print(df_email.loc['hharridge1@gnu.org'].values.tolist()) #list
+
+# a lot of email
+print(df_email.loc[['hharridge1@gnu.org','kbeasant4@jigsy.com']]) #dataframe
