ea3ed7c661ee1b76f6325e741211edf73808d654
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-06 16:44:26 +0200

    add  modifications on 03_select_data.py

--- 03_select_data.py
+++ 03_select_data.py
@@ -21,11 +21,17 @@ print(type(df)) #<class 'pandas.DataFrame'>
 #Serie
 print(type(df.email)) #<class 'pandas.Series'>
 
+#data from 10 to 20 lines
+print(df[10:20]) #line 20 is not in the list
 
+#with LOC with index numeric
+print(df.loc[10:20]) #line 20 is in the list
 
-
-
-
+#with LOC with index varchar
+df_email=df.set_index("email")
+print(df_email.head(10))
+print(df_email.loc['hharridge1@gnu.org']) #
+print(type(df_email.loc['hharridge1@gnu.org'])) #<class 'pandas.Series'>
 
 
 
